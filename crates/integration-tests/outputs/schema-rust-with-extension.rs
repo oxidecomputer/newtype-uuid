8039cba880db1a43194add11d249785e7e2ae8bd
Adam H. Leventhal <ahl@oxide.computer>
2024-05-13 15:18:57 -0700

    use newly-published typify

---
+++ crates/integration-tests/outputs/schema-rust-with-extension.rs
@@ -0,0 +1,54 @@
+/// Error types.
+pub mod error {
+    /// Error from a TryFrom or FromStr implementation.
+    pub struct ConversionError(std::borrow::Cow<'static, str>);
+    impl std::error::Error for ConversionError {}
+    impl std::fmt::Display for ConversionError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+            std::fmt::Display::fmt(&self.0, f)
+        }
+    }
+    impl std::fmt::Debug for ConversionError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+            std::fmt::Debug::fmt(&self.0, f)
+        }
+    }
+    impl From<&'static str> for ConversionError {
+        fn from(value: &'static str) -> Self {
+            Self(value.into())
+        }
+    }
+    impl From<String> for ConversionError {
+        fn from(value: String) -> Self {
+            Self(value.into())
+        }
+    }
+}
+///MyPathStruct
+///
+/// <details><summary>JSON schema</summary>
+///
+/// ```json
+///{
+///  "title": "MyPathStruct",
+///  "type": "object",
+///  "required": [
+///    "id"
+///  ],
+///  "properties": {
+///    "id": {
+///      "$ref": "#/definitions/TypedUuidForMyKind"
+///    }
+///  }
+///}
+/// ```
+/// </details>
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct MyPathStruct {
+    pub id: ::newtype_uuid::TypedUuid<::my_crate::MyKind>,
+}
+impl From<&MyPathStruct> for MyPathStruct {
+    fn from(value: &MyPathStruct) -> Self {
+        value.clone()
+    }
+}
