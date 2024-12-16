e428ab21759920613bc7e4b2eb67000dae1e53ba
Andrew J. Stone <andrew@oxidecomputer.com>
2024-12-16 21:30:19 +0000

    exclude diffus from no-std tests

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -6,7 +6,7 @@ set -e -o pipefail
 
 CARGO="${CARGO:-cargo}"
 EXCLUDED_FEATURES_BY_DEFAULT=(internal-schemars08-tests)
-EXCLUDED_FEATURES_NO_STD=(schemars08 serde default std alloc v4)
+EXCLUDED_FEATURES_NO_STD=(schemars08 serde default std alloc v4 diffus)
 
 trap 'echo_err "Error occurred at $0 command: $BASH_COMMAND"' ERR
 trap 'echo_err "Exiting $0"' EXIT
