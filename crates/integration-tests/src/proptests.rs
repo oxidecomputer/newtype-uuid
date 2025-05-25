22d39a2515eff79b3e13229bd092ba511bbbcd9d
Rain <rain@oxide.computer>
2025-05-24 20:10:12 -0700

    add AsRef<[u8]> and conversion to Vec<u8> (#67)
    Match the uuid crate.

--- crates/integration-tests/src/proptests.rs
+++ crates/integration-tests/src/proptests.rs
@@ -1,6 +1,6 @@
 //! Tests for the property-based infrastructure.
 
-use newtype_uuid::{TypedUuid, TypedUuidKind, TypedUuidTag};
+use newtype_uuid::{GenericUuid, TypedUuid, TypedUuidKind, TypedUuidTag};
 use test_strategy::proptest;
 use uuid::Version;
 
@@ -18,4 +18,16 @@ impl TypedUuidKind for MyKind {
 #[proptest]
 fn prop_is_valid_v4(uuid: TypedUuid<MyKind>) {
     assert_eq!(uuid.get_version(), Some(Version::Random));
+}
+
+/// Ensure that AsRef and conversions to Vec<u8> match Uuid.
+#[proptest]
+fn prop_as_bytes(typed_uuid: TypedUuid<MyKind>) {
+    let typed_uuid_ref: &[u8] = typed_uuid.as_ref();
+    let untyped_uuid_ref: &[u8] = typed_uuid.as_untyped_uuid().as_ref();
+    assert_eq!(typed_uuid_ref, untyped_uuid_ref);
+
+    let typed_uuid_vec: Vec<u8> = typed_uuid.into();
+    let untyped_uuid_vec: Vec<u8> = typed_uuid.into_untyped_uuid().into();
+    assert_eq!(typed_uuid_vec, untyped_uuid_vec);
 }
