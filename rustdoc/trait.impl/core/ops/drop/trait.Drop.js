3a9368eb052c673af10d2f9f0adf88f60db0ea2c
sunshowers <rain@sunshowers.io>
2025-05-25 03:13:09 +0000

    Deploying to gh-pages from @ oxidecomputer/newtype-uuid@75c1cb5a02547d46580618c4679ea75916df2193 🚀

---
+++ rustdoc/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["regex_syntax",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"enum\" href=\"regex_syntax/ast/enum.Ast.html\" title=\"enum regex_syntax::ast::Ast\">Ast</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"enum\" href=\"regex_syntax/ast/enum.ClassSet.html\" title=\"enum regex_syntax::ast::ClassSet\">ClassSet</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"regex_syntax/hir/struct.Hir.html\" title=\"struct regex_syntax::hir::Hir\">Hir</a>"]]],["serde_derive_internals",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"serde_derive_internals/struct.Ctxt.html\" title=\"struct serde_derive_internals::Ctxt\">Ctxt</a>"]]],["syn",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"syn/parse/struct.ParseBuffer.html\" title=\"struct syn::parse::ParseBuffer\">ParseBuffer</a>&lt;'a&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[804,304,301]}
---
+++ rustdoc/trait.impl/schemars/trait.JsonSchema.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["newtype_uuid",[["impl&lt;T&gt; <a class=\"trait\" href=\"schemars/trait.JsonSchema.html\" title=\"trait schemars::JsonSchema\">JsonSchema</a> for <a class=\"struct\" href=\"newtype_uuid/struct.TypedUuid.html\" title=\"struct newtype_uuid::TypedUuid\">TypedUuid</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"newtype_uuid/trait.TypedUuidKind.html\" title=\"trait newtype_uuid::TypedUuidKind\">TypedUuidKind</a> + <a class=\"trait\" href=\"schemars/trait.JsonSchema.html\" title=\"trait schemars::JsonSchema\">JsonSchema</a>,</div>"]]],["schemars",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[561,16]}
