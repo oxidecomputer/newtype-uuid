890faa6300c4d8cf8e8ce87f6f2832597d7c23be
Rain <rain@oxide.computer>
2025-06-10 23:10:46 +0000

    [spr] initial version
    Created using spr 1.3.6-beta.1

--- crates/integration-tests/outputs/schema-rust-with-replace.rs
+++ crates/integration-tests/outputs/schema-rust-with-replace.rs
@@ -1,27 +1,59 @@
-///MyPathStruct
+/// Error types.
+pub mod error {
+    /// Error from a `TryFrom` or `FromStr` implementation.
+    pub struct ConversionError(::std::borrow::Cow<'static, str>);
+    impl ::std::error::Error for ConversionError {}
+    impl ::std::fmt::Display for ConversionError {
+        fn fmt(
+            &self,
+            f: &mut ::std::fmt::Formatter<'_>,
+        ) -> Result<(), ::std::fmt::Error> {
+            ::std::fmt::Display::fmt(&self.0, f)
+        }
+    }
+    impl ::std::fmt::Debug for ConversionError {
+        fn fmt(
+            &self,
+            f: &mut ::std::fmt::Formatter<'_>,
+        ) -> Result<(), ::std::fmt::Error> {
+            ::std::fmt::Debug::fmt(&self.0, f)
+        }
+    }
+    impl From<&'static str> for ConversionError {
+        fn from(value: &'static str) -> Self {
+            Self(value.into())
+        }
+    }
+    impl From<String> for ConversionError {
+        fn from(value: String) -> Self {
+            Self(value.into())
+        }
+    }
+}
+///`MyPathStruct`
 ///
 /// <details><summary>JSON schema</summary>
 ///
 /// ```json
-/**{
-  "title": "MyPathStruct",
-  "type": "object",
-  "required": [
-    "id"
-  ],
-  "properties": {
-    "id": {
-      "$ref": "#/definitions/TypedUuidForMyKind"
-    }
-  }
-}*/
+///{
+///  "title": "MyPathStruct",
+///  "type": "object",
+///  "required": [
+///    "id"
+///  ],
+///  "properties": {
+///    "id": {
+///      "$ref": "#/definitions/TypedUuidForMyKind"
+///    }
+///  }
+///}
 /// ```
 /// </details>
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(::serde::Deserialize, ::serde::Serialize, Clone, Debug)]
 pub struct MyPathStruct {
     pub id: ::newtype_uuid::TypedUuid<::my_crate::MyKind>,
 }
-impl From<&MyPathStruct> for MyPathStruct {
+impl ::std::convert::From<&MyPathStruct> for MyPathStruct {
     fn from(value: &MyPathStruct) -> Self {
         value.clone()
     }
